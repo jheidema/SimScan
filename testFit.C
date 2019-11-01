8bd914318615a19c7e4071c9b550191d08cced2a
jheidema <jheid1205@gmail.com>
2019-11-01 09:49:34 -0400

    Adding neutron response function for qdc cut and short script to test fitting for different energies

---
+++ testFit.C
@@ -0,0 +1,23 @@
+#include "TH1F.h"
+#include "TF1.h"
+#include "nResponse.hpp"
+
+
+void testFit(TH1F *hIn, Bool_t kFit){
+
+    double xMax = hIn->GetBinCenter(hIn->GetMaximumBin());
+    double yMax = hIn->GetBinContent(hIn->GetMaximumBin());
+    double upperX = hIn->GetBinCenter(hIn->GetNbinsX());
+    TF1 *fTOF = new TF1("f1",nResponse,0,1000,11);
+    fTOF->SetNpx(1000);
+    //fTOF->SetParameter(0,xMax);
+    //fTOF->SetParameter(10,yMax);
+    cout << "Fitting Response around " << xMax << endl;
+    
+    hIn->Draw();
+    fTOF->SetParameters(xMax,0.02,1.5,2.0,0.09,2.5,0.13,8.2,0.021,17.5,yMax);
+    if(kFit) hIn->Fit(fTOF,"","",xMax-20,upperX);
+        else fTOF->Draw("same");
+
+    return;
+}
